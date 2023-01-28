b2481df61baf46a4849fcf79d0facb405fe763a9
Jeroen Heremans <jeroenheremans7@gmail.com>
2023-01-28 13:14:40 +0100

    3. Building the new task form

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -7,10 +7,18 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Tasker.Pages
 {
-	public class CreateTaskModel : PageModel
+    public class CreateTaskModel : PageModel
     {
+        [BindProperty]
+        public Task NewTask { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("Index");
         }
     }
 }
